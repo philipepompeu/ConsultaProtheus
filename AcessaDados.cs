3e05499f3914d400c9dbe3c852c0757478e2cf58
Philipe Pompeu <philipepompeu2@gmail.com>
2017-08-08 10:52:44 -0300

    Initial commit

---
+++ AcessaDados.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaProtheus
+{
+    public class AcessaDados
+    {
+        private String connectionStringWindows ="Data Source=192.168.0.12;" +
+                                                "Initial Catalog=P11_SQA;" +
+                                                "Integrated Security=SSPI;";
+
+        private String connectionString =   "Data Source=192.168.0.12;" +
+                                            "Initial Catalog=P11_SQA;" +
+                                            "User id=sa;" +
+                                            "Password=sa#sigla;";
+
+        private SqlConnection connection;
+        public AcessaDados()
+        {
+
+        }
+
+        public void desconectaDoBanco()
+        {
+            this.connection.Close();
+            Console.WriteLine("Desconexão bem sucedida.");
+        }
+
+        public bool conectaAoBanco(){
+            bool resultado = false;
+            this.connection = new SqlConnection();
+
+            this.connection.ConnectionString = this.connectionString;
+            try
+            {
+                this.connection.Open();
+                resultado = true;
+                Console.WriteLine("Conexao bem sucedida.");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Conexao falhou:");
+                Console.WriteLine(e.Message);
+            }
+            return resultado;
+            
+        }
+
+        public SqlDataReader executaConsulta(String consulta)
+        {
+            Console.WriteLine("Consultando...");
+            return new SqlCommand(consulta,this.connection).ExecuteReader();
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,53 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsultaProtheus
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            try
+            {
+                Console.ForegroundColor = ConsoleColor.Green;
+                Console.WriteLine("Aperte <Enter> para iniciar");
+                if (Console.ReadKey().Key == ConsoleKey.Enter)
+                {
+                    Console.WriteLine("Iniciando processamento...");
+
+                    AcessaDados conexao = new AcessaDados();
+
+                    if (conexao.conectaAoBanco())
+                    {
+                        String query = "select top 10 A1_NREDUZ from SA1020 order by R_E_C_N_O_ DESC";
+
+                        SqlDataReader resultado = conexao.executaConsulta(query);
+
+                        while (resultado.Read())
+                        {
+                            Console.WriteLine("Nome:{0}",resultado[0]);
+                        }
+
+                        conexao.desconectaDoBanco();
+                    }
+
+                }
+
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Um erro inesperado ocorreu. Log abaixo:");
+                Console.WriteLine(e.Message);
+            }
+            finally
+            {
+                Console.WriteLine("Aperte qualquer tecla para sair...");
+                Console.ReadKey();
+            }
+        }
+    }
+}
